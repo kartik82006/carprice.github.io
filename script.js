b51ea951d02626422103dc43c0449ed1fed23a96
Kartik <66481305+kartik82006@users.noreply.github.com>
2025-02-23 02:08:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,118 @@
+const element = document.getElementsByClassName("card-item");
+const smalltext = document.getElementsByName('ytext');
+
+const element1 = document.getElementsByClassName("benefit-item");
+
+    // Add class on hover (mouseenter)
+    
+      element[0].addEventListener("mouseenter", function(){
+      smalltext[1].classList.add("get-yellow");
+      console.log("hi");
+      
+    });
+
+    // Remove class when hover ends (mouseleave)
+    element[0].addEventListener("mouseleave", function() {
+      smalltext[1].classList.remove("get-yellow");
+    });
+    
+    element[1].addEventListener("mouseenter", function(){
+      smalltext[2].classList.add("get-yellow");
+      console.log("hi");
+      
+    });
+
+    // Remove class when hover ends (mouseleave)
+    element[1].addEventListener("mouseleave", function() {
+      smalltext[2].classList.remove("get-yellow");
+    });
+    element[2].addEventListener("mouseenter", function(){
+      smalltext[3].classList.add("get-yellow");
+      console.log("hi");
+      
+    });
+
+    // Remove class when hover ends (mouseleave)
+    element[2].addEventListener("mouseleave", function() {
+      smalltext[3].classList.remove("get-yellow");
+    });
+    
+    element[3].addEventListener("mouseenter", function(){
+      smalltext[4].classList.add("get-yellow");
+      console.log("hi");
+      
+    });
+
+    // Remove class when hover ends (mouseleave)
+    element[3].addEventListener("mouseleave", function() {
+      smalltext[4].classList.remove("get-yellow");
+    });
+
+
+
+    element1[0].addEventListener("mouseenter", function(){
+      smalltext[5].classList.add("get-yellow");
+      console.log("hi");
+      
+    });
+
+    // Remove class when hover ends (mouseleave)
+    element1[0].addEventListener("mouseleave", function() {
+      smalltext[5].classList.remove("get-yellow");
+    });
+
+    element1[1].addEventListener("mouseenter", function(){
+      smalltext[6].classList.add("get-yellow");
+      console.log("hi");
+      
+    });
+
+    // Remove class when hover ends (mouseleave)
+    element1[1].addEventListener("mouseleave", function() {
+      smalltext[6].classList.remove("get-yellow");
+    });
+    
+
+    element1[2].addEventListener("mouseenter", function(){
+      smalltext[7].classList.add("get-yellow");
+      console.log("hi");
+      
+    });
+
+    // Remove class when hover ends (mouseleave)
+    element1[2].addEventListener("mouseleave", function() {
+      smalltext[7].classList.remove("get-yellow");
+    });
+
+    element1[3].addEventListener("mouseenter", function(){
+      smalltext[8].classList.add("get-yellow");
+      console.log("hi");
+      
+    });
+
+    // Remove class when hover ends (mouseleave)
+    element1[3].addEventListener("mouseleave", function() {
+      smalltext[8].classList.remove("get-yellow");
+    });
+    
+    element1[4].addEventListener("mouseenter", function(){
+      smalltext[9].classList.add("get-yellow");
+      console.log("hi");
+      
+    });
+
+    // Remove class when hover ends (mouseleave)
+    element1[4].addEventListener("mouseleave", function() {
+      smalltext[9].classList.remove("get-yellow");
+    });
+
+    element1[5].addEventListener("mouseenter", function(){
+      smalltext[10].classList.add("get-yellow");
+      console.log("hi");
+      
+    });
+
+    // Remove class when hover ends (mouseleave)
+    element1[5].addEventListener("mouseleave", function() {
+      smalltext[10].classList.remove("get-yellow");
+    });
